945fcfac5243606cdb86874c404056aedefe0aea
Valentina Klochkova <KlochkovaValentina11@yandex.ru>
2024-01-12 12:12:00 +0300

    change 12/01/24

--- src/Main.java
+++ src/Main.java
@@ -11,41 +11,18 @@ public class Main {
 
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            threads.add(logic(text));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
         }
         long endTs = System.currentTimeMillis(); // end time
 
         System.out.println("Time: " + (endTs - startTs) + "ms");
-
-        Runnable logic = () -> {
-            for (int i = 0; i < texts.length; i++) {
-            }
-        };
-
-        Thread thread = new Thread(logic);
-        thread.start();
-        for (Thread thread1 : threads) {
-            thread1.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
-        }
-        }
+    }
 
     public static String generateText(String letters, int length) {
         Random random = new Random();
@@ -56,4 +33,29 @@ public class Main {
         return text.toString();
     }
 
+    public static Thread logic(String text) {
+        return new Thread(
+                () -> {
+                    int maxSize = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        for (int j = 0; j < text.length(); j++) {
+                            if (i >= j) {
+                                continue;
+                            }
+                            boolean bFound = false;
+                            for (int k = i; k < j; k++) {
+                                if (text.charAt(k) == 'b') {
+                                    bFound = true;
+                                    break;
+                                }
+                            }
+                            if (!bFound && maxSize < j - i) {
+                                maxSize = j - i;
+                            }
+                        }
+                    }
+                    System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                }
+        );
+    }
 }
